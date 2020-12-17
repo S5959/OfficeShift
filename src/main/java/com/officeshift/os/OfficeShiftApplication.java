c287b603963d4ed366225e86f748f6014dc94845
CSM <tbehd8967@naver.com>
2020-12-17 16:51:09 +0900

    Initial commit

---
+++ src/main/java/com/officeshift/os/OfficeShiftApplication.java
@@ -0,0 +1,13 @@
+package com.officeshift.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeShiftApplication.class, args);
+	}
+
+}
